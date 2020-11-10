d755924aebe3aec836cda501c480afe34a21aa57
Maxime <35689674+MaximeThm@users.noreply.github.com>
2020-11-10 14:28:37 +0100

    Update main_script.py
    Improved the code to be more user friendly

--- main_script.py
+++ main_script.py
@@ -1,15 +1,16 @@
 # Import all dependencies
 import imaplib
 import email
+from tkinter.filedialog import askdirectory
 
 # Email credentials
-user_mail = 'xxx'
-mdp_mail = 'xxx'
+user_mail = input("What is your email address? :")
+mdp_mail = input("What is your email password? :")
 
 # IMAP server used
 server = 'imap-mail.outlook.com'
-# Path to the temp folder used
-path = 'xxx'
+# Path to the download folder
+path = askdirectory(title='Select Folder')  # shows dialog box and return the path
 
 
 # Download attachments from an IMAP web server
@@ -18,6 +19,8 @@ def connect(server, user_mail, mdp_mail):
     m.login(user_mail, mdp_mail)
     m.select()
     return m
+
+
 def download(m, emailid, path):
     resp, data = m.fetch(emailid, "(BODY.PEEK[])")
     email_body = data[0][1]
@@ -27,6 +30,8 @@ def download(m, emailid, path):
     for part in mail.walk():
         if part.get_content_maintype() != 'multipart' and part.get('Content-Disposition') is not None:
             open(path + '/' + part.get_filename(), 'wb').write(part.get_payload(decode=True))
+
+
 def query():
     m = connect(server, user_mail, mdp_mail)
     m.select("Inbox")
@@ -35,3 +40,6 @@ def query():
     for emailid in msgs:
         download(m, emailid, path)
         m.store(emailid, '+FLAGS', '\\Seen')
+
+
+query()
